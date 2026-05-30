7b0fac4ab5be589d37e464df702b86df8e5a41c6
Emerson <filho198508@gmail.com>
2026-05-29 22:54:04 -0300

    fix correção3.0

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// Se o Vite não ler do .env, ele usa os valores direto aqui como segurança
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://lljufgrciitltngpseid.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInN1YiI6ImxsanVmZ3JjaWl0bHRuZ3BzZWlkIiwiaWF0IjoxNzE1cU9iZUhSZXZ3QnZlZmxraWljbTlzWlNJNkltRnViMjRsY0pweFFpT2pFM09EQXdOTA4M4MjuImV4cCI6MjNfgyNX0.WSQ-Ou_QME9WXPEeV1658P3eoK-YFkDjEi-G4qc-HOk'
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
-
-console.log("Supabase conectado:", supabase)
+export const supabase = createClient(supabaseUrl, supabaseKey)
